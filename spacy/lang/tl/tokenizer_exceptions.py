3cead7d1dbf0b3860e52f29c88b3365bb9b7486f
Lj Miranda <ljvmiranda@gmail.com>
2023-07-17 14:49:05 +0800

    Put tl pronouns in a single rule
    Anything that ends in a vowel can be contracted to 't or 'y.
    For example:
    
    Ako'y pupunta sa Baguio -> Ako ay pupunta sa Baguio
    I'm going to Baguio -> I am going to Baguio

--- spacy/lang/tl/tokenizer_exceptions.py
+++ spacy/lang/tl/tokenizer_exceptions.py
@@ -3,16 +3,24 @@ from ...util import update_exc
 from ..tokenizer_exceptions import BASE_EXCEPTIONS
 
 _exc = {
-    "tayo'y": [{ORTH: "tayo"}, {ORTH: "'y", NORM: "ay"}],
     "isa'y": [{ORTH: "isa"}, {ORTH: "'y", NORM: "ay"}],
     "baya'y": [{ORTH: "baya"}, {ORTH: "'y", NORM: "ay"}],
     "sa'yo": [{ORTH: "sa"}, {ORTH: "'yo", NORM: "iyo"}],
     "ano'ng": [{ORTH: "ano"}, {ORTH: "'ng", NORM: "ang"}],
-    "siya'y": [{ORTH: "siya"}, {ORTH: "'y", NORM: "ay"}],
     "nawa'y": [{ORTH: "nawa"}, {ORTH: "'y", NORM: "ay"}],
     "papa'no": [{ORTH: "papa'no", NORM: "papaano"}],
     "'di": [{ORTH: "'di", NORM: "hindi"}],
 }
+
+# Pronouns
+for pron in ["ako", "tayo", "kami", "kayo", "siya", "sila", "kanya", "kanila"]:
+    for orth in [pron, pron.title()]:
+        _exc[orth + "'y"] = [
+            {ORTH: orth, NORM: pron},
+            {ORTH: "'y", NORM: "ay"},
+        ]
+
+
 
 
 TOKENIZER_EXCEPTIONS = update_exc(BASE_EXCEPTIONS, _exc)
