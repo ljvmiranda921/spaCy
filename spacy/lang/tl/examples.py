0bbd1a06d2d321edb56eef3cbf6cca429373ea1c
Lj Miranda <ljvmiranda@gmail.com>
2023-07-17 15:31:08 +0800

    Add examples file for testing

---
+++ spacy/lang/tl/examples.py
@@ -0,0 +1,18 @@
+"""
+Example sentences to test spaCy and its language models.
+
+>>> from spacy.lang.tl.examples import sentences
+>>> docs = nlp.pipe(sentences)
+"""
+
+
+sentences = [
+    "Iniisip ng Apple na bilhin ang isang U.K. startup sa halagang isang bilyong dolyar.",
+    "Nililipat ng mga awtonomong sasakyan ang pananagutan sa mga pabrika.",
+    "Pinagmumunihan ng San Francisco ang pagbabawal sa mga robot na naghahatid sa bangketa",
+    "Isang malaking lungsod ang London sa United Kingdom."
+    "Nasaan ka?",
+    "Sino ang presidente ng Pransiya?",
+    "Ano ang kabisera ng Estados Unidos?",
+    "Kailan ipinanganak si Barack Obama?",
+]
