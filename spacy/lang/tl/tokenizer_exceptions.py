559824cd5181d6a60e7dc9fbf477326fb97d0497
Lj Miranda <ljvmiranda@gmail.com>
2023-07-17 15:06:07 +0800

    Add more special cases for tokenization

--- spacy/lang/tl/tokenizer_exceptions.py
+++ spacy/lang/tl/tokenizer_exceptions.py
@@ -7,13 +7,21 @@ _exc = {
     "baya'y": [{ORTH: "baya"}, {ORTH: "'y", NORM: "ay"}],
     "sa'yo": [{ORTH: "sa"}, {ORTH: "'yo", NORM: "iyo"}],
     "ano'ng": [{ORTH: "ano"}, {ORTH: "'ng", NORM: "ang"}],
+    "anong": [{ORTH: "ano"}, {ORTH: "ng", NORM: "ang"}],
+    "ilang": [{ORTH: "ila", NORM: "ilan"}, {ORTH: "ng", NORM: "ang"}],
     "nawa'y": [{ORTH: "nawa"}, {ORTH: "'y", NORM: "ay"}],
     "papa'no": [{ORTH: "papa'no", NORM: "papaano"}],
+    "mong": [{ORTH: "mo"}, {ORTH: "ng", NORM: "nang"}],
+    "bagama't": [{ORTH: "bagama", NORM: "bagaman"}, {ORTH: "'t", NORM: "at"}],
+    "bagama'y": [{ORTH: "bagama", NORM: "bagaman"}, {ORTH: "'y", NORM: "ay"}],
+    # Other uses for apostrophe (kudlit)
     "'di": [{ORTH: "'di", NORM: "hindi"}],
+    "'ko": [{ORTH: "'ko", NORM: "ako"}],
+    "'yo": [{ORTH: "'yo", NORM: "iyo"}],
 }
 
 # Pronouns
-for pron in ["ako", "tayo", "kami", "kayo", "siya", "sila", "kanya", "kanila"]:
+for pron in ["ako", "tayo", "kami", "kayo", "iyo", "siya", "sila", "kanya", "kanila"]:
     for orth in [pron, pron.title()]:
         _exc[orth + "'y"] = [
             {ORTH: orth, NORM: pron},
